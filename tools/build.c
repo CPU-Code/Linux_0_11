620de2a2c551c7aaeb27cba1710363d7e2a2ec48
cpucode <923992029@qq.com>
2020-08-19 18:53:20 +0800

    build__生成内核磁盘映像文件

--- tools/build.c
+++ tools/build.c
@@ -1,4 +1,15 @@
 /*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-08-12 19:19:54
+ * @LastEditTime: 2020-08-18 17:04:10
+ * @FilePath: \Linux_0_11\tools\build.c
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+/*
  *  linux/tools/build.c
  *
  *  (C) 1991  Linus Torvalds
