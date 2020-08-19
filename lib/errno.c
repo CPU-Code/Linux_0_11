8b6f5737fb0d6d62407faf8210a7e80aa86a8a5c
cpucode <923992029@qq.com>
2020-08-19 18:53:13 +0800

    errno__出错号

--- lib/errno.c
+++ lib/errno.c
@@ -1,7 +1,19 @@
 /*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-08-12 19:19:54
+ * @LastEditTime: 2020-08-19 17:48:53
+ * @FilePath: \Linux_0_11\lib\errno.c
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+/*
  *  linux/lib/errno.c
  *
  *  (C) 1991  Linus Torvalds
  */
 
+// 定义了一个出错号变量 errno。用于在函数调用失败时存放出错号
 int errno;
