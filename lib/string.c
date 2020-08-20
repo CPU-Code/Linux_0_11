4042a45eb5526106f78427e98253814c665e5c24
cpucode <923992029@qq.com>
2020-08-20 16:01:46 +0800

    string__空声明

--- lib/string.c
+++ lib/string.c
@@ -1,14 +1,29 @@
 /*
+ * @由于个人水平有限, 难免有些错误, 还请指点:  
+ * @Author: cpu_code
+ * @Date: 2020-08-12 19:19:54
+ * @LastEditTime: 2020-08-20 15:52:07
+ * @FilePath: \Linux_0_11\lib\string.c
+ * @Gitee: [https://gitee.com/cpu_code](https://gitee.com/cpu_code)
+ * @Github: [https://github.com/CPU-Code](https://github.com/CPU-Code)
+ * @CSDN: [https://blog.csdn.net/qq_44226094](https://blog.csdn.net/qq_44226094)
+ * @Gitbook: [https://923992029.gitbook.io/cpucode/](https://923992029.gitbook.io/cpucode/)
+ */
+/*
  *  linux/lib/string.c
  *
  *  (C) 1991  Linus Torvalds
  */
 
-#ifndef __GNUC__
+#ifndef __GNUC__    // 需要 GNU 的 C 编译器编译
 #error I want gcc!
 #endif
 
+//声明'extern'和'inline'前缀为空
 #define extern
 #define inline
+
 #define __LIBRARY__
+
+// 包含 string.h 头文件
 #include <string.h>
